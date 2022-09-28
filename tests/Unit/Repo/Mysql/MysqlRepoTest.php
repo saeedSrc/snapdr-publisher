4cf48821e4d7d581177d8dff5c5a910a2d91e0d7
saeed rasooli <saeedrasooli@saeeds-MacBook-Pro.local>
2022-09-28 17:54:52 +0330

    snap dr assignment

--- tests/Unit/Repo/Mysql/MysqlRepoTest.php
+++ tests/Unit/Repo/Mysql/MysqlRepoTest.php
@@ -12,17 +12,17 @@ class MysqlRepoTest extends TestCase
 {
     use DatabaseTransactions;
 
-//    public function testInsertShouldWork(): void
-//    {
-//        //arrange
-//        $notification = Notification::factory()->make();
-//        $repository = new MysqlNotification\MysqlRepository();
-//
-//        //act
-//        $result = $repository->insert($notification);
-//
-//        //assert
-//        $this->assertDatabaseHas('notifications', $notification->toArray());
-//        $this->assertNotNull($result->id);
-//    }
+    public function testInsertShouldWork(): void
+    {
+        //arrange
+        $notification = Notification::factory()->make();
+        $repository = new MysqlNotification\MysqlRepository();
+
+        //act
+        $result = $repository->insert($notification);
+
+        //assert
+        $this->assertDatabaseHas('notifications', $notification->toArray());
+        $this->assertNotNull($result->id);
+    }
 }
